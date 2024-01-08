c00330647ca8dfef321c13bc539cf1c5a8f099e0
Aswathy755 <aswathyas589@gmail.com>
2024-01-08 18:21:12 +0530

    its my first task

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-console.log(haii);
+let arr=[1,2,5,8,9]
 
-console.log(rv);
+for (let i = 0; i <=arr.length; i++) {
+    let element = arr[i];
+    console.log(element)
+}
+
+let person= { name:'aswathy', age:14, place:'tsr'}
+for(x in person){
+    console.log(person[x]);
+
+}
+
+let arr2=[1,2,3,4,5,6];
+let j=0;
+while(j<arr2.length){
+    console.log(arr2[j]);
+    j++
+}
